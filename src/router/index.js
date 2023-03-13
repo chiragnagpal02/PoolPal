ea8a9f0905086798e5af5ca914427644867eaaf3
tianwanlin <wanlin.tian.2021@scis.smu.edu.sg>
2023-03-14 01:07:03 +0800

    created folders for views

--- src/router/index.js
+++ src/router/index.js
@@ -6,18 +6,7 @@ const routes = [
     name: 'identity',
     component: identity
   },
-  {
-    path: '/home',
-    name: 'home',
-  },
-  {
-    path: '/profile',
-    name: 'profile',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/ProfileView.vue')
-  }
+
 ]
 
 const router = createRouter({
