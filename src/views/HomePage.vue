0a46273097a751ea22fac1fad990800f89175e36
tianwanlin <wanlin.tian.2021@scis.smu.edu.sg>
2023-03-14 11:30:46 +0800

    -

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,11 +1,15 @@
 <template>
-    <div class="container-fluid" id="homepageImage">
-
+    <div class="container-fluid d-flex align-items-center justify-content-center" id="homepageImage">
+        <div>
+            <h1 class="text-white text-center" style="font-size:50px">YOUR CARPOOLING MATTERS.</h1>
+            <h3 class="text-white text-center">Share the Ride, Save the Planet</h3>
+        </div>
     </div>
 </template>
 
-<style>
+<style scoped>
     #homepageImage{
+        padding:20%;
         position: absolute;
         width:100%;
         height:100%;
