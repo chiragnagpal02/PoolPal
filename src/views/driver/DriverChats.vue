7ec845e5f34cfe9f35b610c4b9d85145d8760e73
tianwanlin <wanlin.tian.2021@scis.smu.edu.sg>
2023-03-19 15:03:47 +0800

    driver chat done

--- src/views/driver/DriverChats.vue
+++ src/views/driver/DriverChats.vue
@@ -3,40 +3,53 @@
 
     <div class="container-fluid background">
         <!-- back button -->
-        <div class="container-fluid float-start my-3" >
-            <img src="../../assets/backbutton.png" width="50" height="50">
+        <div class="container" >
+            <img src="../../assets/backbutton.png" width="30" height="30" style="aspect-ratio:1/1;opacity:65%">
         </div>
 
         <!-- chat -->
-        <div class="container bg-white border border-3 mt-5">
+        <div class="container bg-white border border-3 mt-3">
             <!-- receiver -->
             <div class="row text-center">
                 <h3 class="py-4">Receiver Name</h3>
                 <hr>
             </div>
             <!-- messages -->
-            <div class="container">
+            <div class="container message">
                 <img src="../../assets/profile.jpg" alt="Avatar">
                 <p>Hello. How are you today?</p>
                 <span class="time-right">11:00</span>
             </div>
 
-            <div class="container darker">
+            <div class="container message darker">
                 <img src="../../assets/profile.jpg" alt="Avatar" class="right">
                 <p>Hey! I'm fine. Thanks for asking!</p>
                 <span class="time-left">11:01</span>
             </div>
 
-            <div class="container">
+            <div class="container message">
                 <img src="../../assets/profile.jpg" alt="Avatar">
                 <p>Sweet! So, what do you wanna do today?</p>
                 <span class="time-right">11:02</span>
             </div>
 
-            <div class="container darker">
+            <div class="container message darker">
                 <img src="../../assets/profile.jpg" alt="Avatar" class="right">
                 <p>Nah, I dunno. Play soccer.. or learn more coding perhaps?</p>
                 <span class="time-left">11:05</span>
+            </div>
+
+            <!-- send message -->
+
+            <div class="row my-4">
+                <div class="col-10 d-flex justify-content-center align-items-center">
+                    <div class="container sendMessage d-flex">
+                        <h8 class="my-1">send message....</h8>
+                    </div>
+                </div>
+                <div class="col container d-flex justify-content-center align-items-center">
+                    <img src="../../assets/send.png" width="30" height="30" style="opacity:65%;">
+                </div>
             </div>
         </div>
     </div>
@@ -60,7 +73,7 @@
     background-color:honeydew;
     }
     /* Chat containers */
-    .container {
+    .message {
     border: 2px solid #dedede;
     background-color: #f1f1f1;
     border-radius: 5px;
@@ -75,14 +88,14 @@
     }
 
     /* Clear floats */
-    .container::after {
+    .message::after {
     content: "";
     clear: both;
     display: table;
     }
 
     /* Style images */
-    .container img {
+    .message img {
     float: left;
     max-width: 60px;
     width: 100%;
@@ -91,7 +104,7 @@
     }
 
     /* Style the right image */
-    .container img.right {
+    .message img.right {
     float: right;
     margin-left: 20px;
     margin-right:0;
@@ -107,5 +120,11 @@
     .time-left {
     float: left;
     color: #999;
+    }
+
+    .sendMessage{
+        border: 2px solid #dedede;
+        background-color: #f1f1f1;
+        border-radius: 15px;
     }
 </style>
