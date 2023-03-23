458445c9207e0ca99915f66acbb268c2541dc00c
lyw142 <111484155+lyw142@users.noreply.github.com>
2023-03-24 01:58:09 +0800

    Update reviewMS.py and staffMS.py

--- reviewMS.py
+++ reviewMS.py
@@ -3,8 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/review'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/poolpal'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_recycle': 299}
 
 
 db = SQLAlchemy(app)
