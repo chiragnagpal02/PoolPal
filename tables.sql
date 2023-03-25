fa6056c8adc3e7fb276a7a4b270a7165cbcb0fd6
bryanlzh <bryanlzh1@gmail.com>
2023-03-25 23:58:45 +0800

    videoMS

--- tables.sql
+++ tables.sql
@@ -80,4 +80,12 @@ CREATE TABLE User (
     PRIMARY KEY (Email, Role)
 );
 
+CREATE TABLE IF NOT EXISTS users (
+  id INTEGER AUTO_INCREMENT,
+  email varchar(64) NOT NULL,
+  username varchar(64) NOT NULL,
+  password_hash varchar(64) NOT NULL,
+  PRIMARY KEY (id)
+);
+
 
