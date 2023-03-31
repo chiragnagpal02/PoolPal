9292d80a8119188148e439f01b61d5716c58ccd4
Chirag Nagpal <85030123+chiragnagpal02@users.noreply.github.com>
2023-03-31 22:40:48 +0800

    push_changes

--- driverMS.py
+++ driverMS.py
@@ -12,7 +12,7 @@ import json
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:3306/PoolPal' 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://poolpal@localhost:3306/PoolPal' 
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_recycle': 299}
 
 db = SQLAlchemy(app)
