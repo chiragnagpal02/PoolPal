4c1d1cee47da5404a809bc11dfec300395308374
Chirag Nagpal <85030123+chiragnagpal02@users.noreply.github.com>
2023-04-01 14:02:39 +0800

    matching_carpool.py updated

--- match_carpoolsCMS.py
+++ match_carpoolsCMS.py
@@ -2,57 +2,72 @@
 
 from flask import Flask, request, jsonify
 import requests
-from datetime import datetime
+from datetime import datetime, timedelta
 from geopy.distance import geodesic
+
 
 app = Flask(__name__)
 
 THRESHOLD_DISTANC_KMS = 2
-CARPOOLS_URL = 'http://127.0.0.1:5002/get_all_carpools'
+CARPOOLS_URL = 'http://127.0.0.1:5002/api/v1/carpool/get_all_carpools'
 
 # Passenger chooses date, start location and end location
 # Have to check for 3 factors -> start location,  and date
 
-def get_all_existing_carpools(start_lat, start_lng):
+def get_all_existing_carpools(start_lat, start_lng, end_lat, end_lng, formatted_date, start_time_str):
     response = requests.get(CARPOOLS_URL)
     statuses = {}
-    passenger_coords = (start_lat, start_lng)
+    passenger_start_coords = (start_lat, start_lng)
+    passenger_end_coords = (end_lat, end_lng)
+    passenger_start_time = datetime.strptime(start_time_str, "%H:%M:%S").time()
+    print(passenger_start_time)
+
     carpools = response.json()['data']['carpools']
     for i in carpools:
+        # Convert carpool date string to datetime object
         date = i['DateTime']
         datetime_obj = datetime.strptime(date, '%a, %d %b %Y %H:%M:%S %Z')
-        datetime_obj.date()
-        print(datetime_obj)
-        # print(d)
+        to_compare_date = datetime_obj.date()
+
+        # Get carpool start time as datetime.time object
+        start_time = datetime_obj.time()
+
+        # Calculate lower and upper bounds of time window
+        lower_time = (datetime.combine(formatted_date, passenger_start_time) - timedelta(hours=2)).time()
+        upper_time = (datetime.combine(formatted_date, passenger_start_time) + timedelta(hours=2)).time()
         
         cpid = i['CPID']
-        start_location = i['CPStartLocation']
-
 
         start_coords = (i['CPStartLatitude'], i['CPStartLongitude'])
-        
-        # print(start_coords)
-        # print(end_coords)
-       
-        distance = match_carpools(start_coords, passenger_coords)
-        print(f"{cpid}: Distance between {start_location} and Passenger Coordinates is {round(distance, 2)} kms!")
-        
-        # check for the threshold distance
+        end_coords = (i['CPEndLatitude'], i['CPEndLongitude'])
 
-        if distance < THRESHOLD_DISTANC_KMS:
+        start_distance = match_carpools(start_coords, passenger_start_coords)
+        end_distance = match_carpools(end_coords, passenger_end_coords)
+        print(f"CARPOOL DATE - {to_compare_date}")
+        print(f"PASSENGER DATE - {formatted_date}")
+        print((to_compare_date == formatted_date))
+        # Check if carpool start and end locations are within 1 km of passenger locations,
+        # carpool date is the same as passenger date, and carpool start time is within 2 hours
+        # of passenger start time
+        if (start_distance < THRESHOLD_DISTANC_KMS) and \
+           (end_distance < THRESHOLD_DISTANC_KMS) and \
+           (to_compare_date == formatted_date) and \
+           (start_time >= lower_time) and \
+           (start_time <= upper_time):
             statuses[cpid] = True
         else:
             statuses[cpid] = False
-    
+
     return carpools, statuses
+
         
 
 def match_carpools(start_coords, end_coords):
     distance = geodesic(start_coords, end_coords).km
     return distance
 
-def send_matching_carpools(start_lat, start_lng):
-    carpools, statuses = get_all_existing_carpools(start_lat, start_lng)
+def send_matching_carpools(start_lat, start_lng, end_lat, end_lng, formatted_date, start_time_str):
+    carpools, statuses = get_all_existing_carpools(start_lat, start_lng, end_lat, end_lng, formatted_date, start_time_str)
     final_carpools = []
     for i in carpools:
         cpid = i['CPID']
@@ -63,9 +78,20 @@ def send_matching_carpools(start_lat, start_lng):
     
     return final_carpools 
 
-@app.route('/api/v1/matching/get_matching_carpools/<float:start_lat>,<float:start_lng>/', methods=['GET'])
-def get_matching_carpools(start_lat, start_lng):
-    carpools = send_matching_carpools(start_lat, start_lng)
+@app.route('/api/v1/matching/get_matching_carpools/', methods=['POST'])
+def get_matching_carpools():
+
+    start_lat = request.json.get('start_lat')
+    start_lng = request.json.get('start_lng')
+    end_lat = request.json.get('end_lat')
+    end_lng = request.json.get('end_lng')
+    Start_time = request.json.get('time')
+    date = request.json.get('date')
+    formatted_date = datetime.strptime(date, "%Y-%m-%d")
+    date_final = formatted_date.date()
+    print(Start_time)
+
+    carpools = send_matching_carpools(float(start_lat), float(start_lng), float(end_lat), float(end_lng), date_final, Start_time)
     print(carpools)
     return jsonify(carpools)
 
