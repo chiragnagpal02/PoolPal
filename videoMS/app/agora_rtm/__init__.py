1c07a8df86d6259a6866867bd29d6af01ae41ad6
bryanlzh <bryanlzh1@gmail.com>
2023-04-04 20:38:30 +0800

    remove video

--- videoMS/app/agora_rtm/__init__.py
+++
@@ -1,4 +0,0 @@
-from flask import Blueprint
-agora_rtm = Blueprint('agora_rtm', '__init__')
-
-from . import views  # isort:skip
--- videoMS/tables.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-  id INTEGER AUTO_INCREMENT,
-  email varchar(64) NOT NULL,
-  username varchar(64) NOT NULL,
-  password_hash varchar(64) NOT NULL,
-  PRIMARY KEY (id)
-);
